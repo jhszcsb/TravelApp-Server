a8da9cb192bcdf3eb4b44d1cb9639449299bd1bb
jhszcsb <jhszcsb@gmail.com>
2015-11-01 19:14:38 +0100

    hateoas test services

--- src/main/java/backend/restcontroller/TripController.java
+++ src/main/java/backend/restcontroller/TripController.java
@@ -3,9 +3,14 @@ package backend.restcontroller;
 import backend.entity.*;
 import backend.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -38,6 +43,50 @@ public class TripController {
     @RequestMapping(value="/{name}/timeline", method=RequestMethod.GET)
     public List<Trip> getAllTripsForTraveler(@PathVariable String name) {
         return tripService.findAllTripsOfFriendsForTraveler(name);
+    }
+
+    // HATEOAS test
+    @RequestMapping(value = "/hateoas", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Resource<Trip> getHateoas() {
+        Trip trip = tripService.findAllForTraveler(1).get(0);
+        Resource<Trip> resource = new Resource(trip);
+        resource.add(linkTo(methodOn(TripController.class).getHateoas()).withSelfRel());
+        return resource;
+    }
+
+    // HATEOAS test for collection
+    @RequestMapping(value = "/hateoas/collection", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<Resource<Trip>> getHateoasCollection() {
+        List<Trip> trips = tripService.findAllForTraveler(1);
+        List<Resource<Trip>> resources = new ArrayList<Resource<Trip>>();
+        for(Trip trip : trips) {
+            resources.add(createTripResource(trip));
+
+        }
+        return resources;
+    }
+
+    private Resource<Trip> createTripResource(Trip trip) {
+        Resource<Trip> resource = new Resource<Trip>(trip);
+        resource.add(linkTo(methodOn(TripController.class).getHateoas()).withSelfRel());
+
+        // other optional links...
+        //resource.add(linkTo(methodOn(TripController.class). .withRel("..."));
+        // Option to purchase Album
+        //if (/*something*/) {
+        //    resource.add(linkTo(methodOn(TripController.class).doSomething(trip.getId())).withRel("trip.something"));
+        //}
+        return resource;
+    }
+
+    // no HATEOAS test
+    @RequestMapping(value = "/nohateoas", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Trip getNoHateoas() {
+        Trip trip = tripService.findAllForTraveler(1).get(0);
+        return trip;
     }
 
     // UPDATE (by Traveler id and Trip id)
